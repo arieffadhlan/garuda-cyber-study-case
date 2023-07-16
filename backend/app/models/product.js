20531bba1e286c0084baab1f1d8d7eb076c78f7b
Muhammad Arief Fadhlan <ariffadhlan12@gmail.com>
2023-07-16 09:44:00 +0700

    (backend) feat: add product model and migration

---
+++ backend/app/models/product.js
@@ -0,0 +1,30 @@
+"use strict";
+const { Model } = require("sequelize");
+const { v4: uuidv4 } = require("uuid");
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    static associate(models) {
+      // define association here
+    }
+  }
+  Product.init({
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4
+    },
+    name: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    price: DataTypes.INTEGER,
+    stock: DataTypes.INTEGER,
+    image: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: "Product",
+  });
+  Product.beforeCreate((product) => {
+    product.id = uuidv4();
+  });
+  return Product;
+};
