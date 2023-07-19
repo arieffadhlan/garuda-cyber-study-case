ecd1729b0894ef3835bfd014f8d71b4dfdf7a52f
Muhammad Arief Fadhlan <ariffadhlan12@gmail.com>
2023-07-19 10:51:52 +0700

    (frontend) feat: update order history

--- frontend/src/components/templates/OrderHistoryList.jsx
+++ frontend/src/components/templates/OrderHistoryList.jsx
@@ -23,42 +23,18 @@ const OrderHistoryList = () => {
     <div className="flex flex-col gap-4">
       {userTransactions.length > 0 && userTransactions.map((transaction) => (
         <div key={transaction.id} className="flex flex-col bg-neutral-1 border rounded-lg shadow-sm">
-          <div className="flex justify-between items-center px-4 py-3 border-b">
-            <div className="flex flex-col gap-2.5 md:flex-row md:gap-9">
-              <div className="flex flex-col gap-0.5 text-sm">
-                <span className="font-semibold">Order Number:</span>
-                <span className="font-medium text-[#595959]">
-                  {transaction.code}
+          <div className="flex flex-col gap-2.5 px-4 py-3 border-b 2xs:flex-row 2xs:gap-9">
+            <div className="flex flex-col gap-0.5 text-sm">
+              <span className="font-medium">Order Number:</span>
+              <span className="font-normal text-[#595959]">
+                {transaction.code}
+              </span>
+            </div>
+            <div className="flex flex-col gap-0.5 text-sm">
+              <span className="font-medium">Order Date:</span>
+              <span className="font-normal text-[#595959]">
+                {dayjs(transaction.createdAt).format("DD MMMM YYYY")}
                 </span>
-              </div>
-              <div className="flex flex-col gap-0.5 text-sm">
-                <span className="font-semibold">Order Date:</span>
-                <span className="font-medium text-[#595959]">
-                  {dayjs(transaction.createdAt).format("DD MMMM YYYY")}
-                  </span>
-              </div>
-              <div className="flex flex-col gap-0.5 text-sm">
-                <span className="font-semibold">Tax:</span>
-                <span className="font-medium text-[#595959]">
-                  Rp {
-                    transaction.orders.reduce((nextValue, current) => {
-                        return nextValue + ((current.quantity * current.product.price) * (10/100))
-                    }, 0).toLocaleString("id-ID")
-                  }
-                </span>
-              </div>
-              <div className="flex flex-col gap-0.5 text-sm">
-                <span className="font-semibold">Total:</span>
-                <span className="font-bold text-emerald-700">
-                  Rp {transaction.ammount.toLocaleString("id-ID")}
-                </span>
-              </div>
-              {transaction?.voucher?.code && (
-                <div className="flex flex-col gap-0.5 text-sm">
-                  <span className="font-semibold">Voucher:</span>
-                  <span className="font-bold text-emerald-700">{transaction.voucher.code}</span>
-                </div>
-              )}
             </div>
           </div>
           <div className="flex flex-col px-4 divide-y divide-gray-200">
@@ -74,15 +50,42 @@ const OrderHistoryList = () => {
                 <div className="flex flex-col gap-0.5 w-full">
                   <div className="flex flex-col text-sm xs:flex-row xs:justify-between xs:items-center xs:text-base">
                     <span className="font-medium">{item.product.name}</span>
-                    <strong className="font-bold">Rp {(item.quantity * item.product.price).toLocaleString("id-ID")}</strong>
+                    <strong className="font-bold">
+                      Rp {(item.quantity * item.product.price).toLocaleString("id-ID")}
+                    </strong>
                   </div>
-                  <span className="text-xs text-[#595959] xs:text-sm">Quantity: {item.quantity}</span>
+                  <span className="text-xs text-[#595959] xs:text-sm">
+                    Quantity: {item.quantity}
+                  </span>
                 </div>
               </div>
             ))}
           </div>
-          <div className="flex justify-end px-4 py-3 border-t">
-            <Button>View Invoice</Button>
+          <div className="flex justify-end items-center px-4 py-3 border-t">
+            <div className="flex flex-col gap-2.5 sm:flex-row sm:gap-9">
+              <div className="flex flex-col gap-0.5 text-sm">
+                <span className="font-medium">Tax:</span>
+                <span className="font-normal text-[#595959]">
+                  Rp {
+                    transaction.orders.reduce((nextValue, current) => {
+                        return nextValue + ((current.quantity * current.product.price) * (10/100))
+                    }, 0).toLocaleString("id-ID")
+                  }
+                </span>
+              </div>
+              <div className="flex flex-col gap-0.5 text-sm">
+                <span className="font-medium">Total:</span>
+                <span className="font-bold text-emerald-700">
+                  Rp {transaction.ammount.toLocaleString("id-ID")}
+                </span>
+              </div>
+              {transaction?.voucher?.code && (
+                <div className="flex flex-col gap-0.5 text-sm">
+                  <span className="font-medium">Voucher:</span>
+                  <span className="font-bold text-emerald-700">{transaction.voucher.code}</span>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       ))}
